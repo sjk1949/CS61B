8b4be25eb7cbd656e41ef258ad27680119fb3bed
sjk1949 <sjkenterprise@163.com>
2024-10-03 01:21:07 -0700

    Get back the correct lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,26 +2,23 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-    }
-
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
         while (n != 1) {
-            n = nextNumber(n);
             System.out.print(n + " ");
+            n = nextNumber(n);
         }
-        System.out.println();
+        System.out.print(n + " ");
+    }
+
+    /** return the next number
+     * @param n int this number*/
+    public static int nextNumber(int n) {
+        if(n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
+        }
     }
 }
 
