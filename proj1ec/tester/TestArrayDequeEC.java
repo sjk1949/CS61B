bb78134b72f0454accc4035c8f00aa31bf646815
sjk1949 <sjkenterprise@163.com>
2024-10-03 00:13:57 -0700

    Fix the change that uncommit

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,2 +1,22 @@
-package tester;public class TestArrayDequeEC {
+package tester;
+
+import static org.junit.Assert.*;
+
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void dequeTest() {
+        StudentArrayDeque<Integer> L = new StudentArrayDeque<>();
+        int N = 100000;
+        for (int i = 0; i < N; i++) {
+            int select = StdRandom.uniform(5);
+            if (select == 0) {
+                L.addFirst();
+            }
+        }
+    }
 }
