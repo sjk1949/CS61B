30cc7322e4a6eda73877e5c7b1e731670fffa46e
sjk1949 <sjkenterprise@163.com>
2024-10-03 01:07:38 -0700

    Fix print error in lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -16,6 +16,7 @@ public class Collatz {
 
     public static void main(String[] args) {
         int n = 5;
+        System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
