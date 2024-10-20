a2acaa4f44ea126f9261530c536b1e0524931bff
sjk1949 <sjkenterprise@163.com>
2024-10-20 16:08:16 -0700

    Finish lab7 basic

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -3,34 +3,50 @@ package bstmap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K, V> implements Map61B<K, V> {
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private BSTNode root;
+    private int size;
+
+    public BSTMap() {
+        this.root = null;
+        this.size = 0;
+    }
 
     public void printInOrder() {
+        inOrderTraversel(this.root);
+        System.out.println();
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        this.root = null;
+        this.size = 0;
     }
 
     @Override
     public boolean containsKey(K Key) {
-        throw new UnsupportedOperationException();
+        return find(this.root, Key) != null;
     }
 
     @Override
     public V get(K Key) {
-        throw new UnsupportedOperationException();
+        BSTNode findResult;
+        if ((findResult = find(this.root, Key)) == null) {
+            return null;
+        }
+        return findResult.value;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return this.size;
     }
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        this.root = insert(this.root, key, value);
+        size += 1;
     }
 
     @Override
@@ -65,5 +81,38 @@ public class BSTMap<K, V> implements Map61B<K, V> {
         }
     }
 
-    private BSTNode searchNode(K key) {}
+    private BSTNode find(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) == 0) {
+            return node;
+        } else if (key.compareTo(node.key) < 0) {
+            return find(node.left, key);
+        } else {
+            return find(node.right, key);
+        }
+    }
+
+    private BSTNode insert(BSTNode node, K key, V value) {
+        if (node == null) {
+            return new BSTNode(key, value);
+        }
+        if (key.compareTo(node.key) == 0) {
+            node.value = value;
+        } else if (key.compareTo(node.key) < 0) {
+            node.left = insert(node.left, key, value);
+        } else {
+            node.right = insert(node.right, key, value);
+        }
+        return node;
+    }
+
+    private void inOrderTraversel(BSTNode node) {
+        if (node != null) {
+            inOrderTraversel(node.left);
+            System.out.print(node.key + " ");
+            inOrderTraversel(node.right);
+        }
+    }
 }
